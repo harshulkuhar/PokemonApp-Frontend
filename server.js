e501470133db25bceb74035def3d5ae03b477eb7
Nonu <harshulkuhar@outlook.com>
2021-02-22 10:43:15 +0530

    added --aot

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./PokemonApp-Frontend/src'));
+app.use(express.static('./dist/app-pokemon/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('PokemonApp-Frontend/src/index.html'),
+    res.sendFile('index.html', {root: 'dist/app-pokemon/'}),
 );
 
 // Start the app by listening on the default Heroku port
