2573085ad13a965bb4e2444b27f0f4c1b74cafce
Nonu <harshulkuhar@outlook.com>
2021-02-22 10:25:14 +0530

    reverted version

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/PokemonApp-Frontend/'));
+app.use(express.static('./src/PokemonApp-Frontend/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/PokemonApp-Frontend/'}),
+    res.sendFile('index.html', {root: 'src/PokemonApp-Frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
